40f332facc6e1bcf4dcec18c340da29ec9b9c832
UNNIdud-tg <UNNIduo@hotmail.com>
2024-10-12 21:00:55 +0530

    Update start.py

--- CodeRunBot/start.py
+++ CodeRunBot/start.py
@@ -1,4 +1,5 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from utils.message import *
 from pyrogram import Client, filters , enums
 
 @Client.on_message(filters.command('start'))
@@ -12,3 +13,7 @@ async def ai_generate_private(client, message):
         text=f"Hey {message.from_user.mention}\n 𝐇𝐞𝐲 𝐭𝐡𝐞𝐫𝐞! 𝐌𝐲 𝐧𝐚𝐦𝐞 𝐢𝐬 𝐕𝐞𝐠𝐞𝐭𝐚 - 𝐈'𝐦 𝐡𝐞𝐫𝐞 𝐭𝐨 𝐡𝐞𝐥𝐩 𝐲𝐨𝐮! \n\n 𝐩𝐫𝐨𝐯𝐢𝐝𝐞𝐝 𝐛𝐲 @MRXSUPPORTS", 
         reply_markup=reply_markup
     )
+
+@Client.on_message(filters.command("help"))
+async def help(client, message):
+    await message.reply_text(HELP)

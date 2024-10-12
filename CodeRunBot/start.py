ec1506a777ce253458eeb87883663fdf3bd80eea
UNNIdud-tg <UNNIduo@hotmail.com>
2024-10-12 22:05:14 +0530

    Update start.py

--- CodeRunBot/start.py
+++ CodeRunBot/start.py
@@ -14,7 +14,7 @@ async def ai_generate_private(client, message):
         text=f"Hey {message.from_user.mention}\n Hey there! My name is Nezuko - I'm here to help you! Use /help to find out more about how to use me to my full potential.\n\nJoin my @MRXSUPPORTS to get information on all the latest updates.",
         reply_markup=reply_markup
     )
-    )
+    
 
 @Client.on_message(filters.command("help"))
 async def help(client, message):

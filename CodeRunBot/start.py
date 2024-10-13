3f6cda789f1b919cf32567fe28b784adb438d5ad
UNNIdud-tg <UNNIduo@hotmail.com>
2024-10-13 10:03:42 +0530

    Update start.py

--- CodeRunBot/start.py
+++ CodeRunBot/start.py
@@ -10,14 +10,14 @@ async def ai_generate_private(client, message):
     ],
     # Second row: Button below the first row
     [
-        InlineKeyboardButton("Contact Us", url="https://t.me/ContactUs")  # New button on a separate row
+        InlineKeyboardButton("Add me to your group", url="http://t.me/NezukoRobot?startgroup=true") # New button on a separate row
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     # Using reply_photo to send an image with a caption
     await message.reply_photo(
-        photo='https://example.com/photo.jpg',  # Replace with the actual image URL
+        photo='https://mangandi-2-0.onrender.com/7EfZ.JPG',  # Replace with the actual image URL
         caption=f"Hey {message.from_user.mention}\n Hey there! My name is Nezuko - I'm here to help you! Use /help to find out more about how to use me to my full potential.\n\nJoin my @MRXSUPPORTS to get information on all the latest updates.",
         reply_markup=reply_markup
     )

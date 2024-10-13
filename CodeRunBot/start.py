dc63950b2c763ce13c01b6a67070b5557b48f891
UNNIdud-tg <UNNIduo@hotmail.com>
2024-10-13 07:30:57 +0530

    Update start.py

--- CodeRunBot/start.py
+++ CodeRunBot/start.py
@@ -6,15 +6,13 @@ from pyrogram import Client, filters , enums
 async def ai_generate_private(client, message):
     buttons = [[
         InlineKeyboardButton("Support", url="https://t.me/MRXSUPPORTS"),
-        InlineKeyboardButton("Add me to your group", url="http://t.me/NezukoRobot?startgroup=true")  # Added a new button
-    ]]
-    buttons = [[
-        InlineKeybordButton("Owner", url="http://t.me/UNNIdud")
+        InlineKeyboardButton("Add me to your group", url="http://t.me/NezukoRobot?startgroup=true")
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
-    # Updated message text
+    
+    # Added line breaks before the buttons for visual separation
     await message.reply_text(
-        text=f"Hey {message.from_user.mention}\n Hey there! My name is Nezuko - I'm here to help you! Use /help to find out more about how to use me to my full potential.\n\nJoin my @MRXSUPPORTS to get information on all the latest updates.",
+        text=f"Hey {message.from_user.mention}\n Hey there! My name is Nezuko - I'm here to help you! Use /help to find out more about how to use me to my full potential.\n\nJoin my @MRXSUPPORTS to get information on all the latest updates.\n\n\n",  # Added extra line breaks
         reply_markup=reply_markup
     )
     
@@ -22,3 +20,4 @@ async def ai_generate_private(client, message):
 @Client.on_message(filters.command("help"))
 async def help(client, message):
     await message.reply_text(HELP)
+    

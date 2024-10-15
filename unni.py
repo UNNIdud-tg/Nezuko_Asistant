319de2a309e5172fdea48db13b09f577b414c306
UNNIdud-tg <UNNIduo@hotmail.com>
2024-10-15 14:31:23 +0530

    Create unni.py

---
+++ unni.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Define command prefixes
+PREFIXES = ['/', '!']  # Adjust command prefixes as needed
+
+# Define the owner ID (replace with your actual user ID)
+OWNER_ID = 6171681404  # Replace with your actual Telegram user ID
